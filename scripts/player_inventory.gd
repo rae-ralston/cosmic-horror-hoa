5401e23c6dd4ab9c69f16ddb65323ba6323960d9
Rachel Ralston <rachel.ralston@kofile.com>
2026-01-20 18:51:45 -0800

    inventory: place items in world

--- scripts/player_inventory.gd
+++ scripts/player_inventory.gd
@@ -26,7 +26,7 @@ func pickup(item: Item) -> bool:
 	
 	# moves under held parent in tree
 	if held_items_parent:
-		item.reparent(held_items_parent)
+		item.reparent(held_items_parent, true)
 	
 	#makes non-interactable while held
 	_set_item_held_state(item, true)
@@ -35,7 +35,7 @@ func pickup(item: Item) -> bool:
 	return true
 
 func drop(drop_global_pos: Vector2, world_parent: Node = null) -> bool:
-	if(held_item == null):
+	if held_item == null:
 		return false
 	
 	var item := held_item
@@ -44,11 +44,15 @@ func drop(drop_global_pos: Vector2, world_parent: Node = null) -> bool:
 	# Reparent/give item back to world instead of player
 	if world_parent:
 		item.reparent(world_parent)
+		# IMPORTANT: After reparenting, we need to set global_position
+		# because reparenting can change the global position calculation
+		item.global_position = drop_global_pos
+	else:
+		# If no world_parent, just set position relative to current parent
+		item.global_position = drop_global_pos
 	
-	item.global_position = drop_global_pos
+	# In player_inventory.gd, in the drop() function, after _set_item_held_state:
 	_set_item_held_state(item, false)
-	
-	emit_signal("held_item_changed", null)
 	return true
 
 func _set_item_held_state(item: Item, is_held: bool) -> void:
